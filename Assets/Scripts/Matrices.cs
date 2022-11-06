ff9f9d3bcd8e20b0e60c574083b4d589c88b8c33
dexfutur <68019118+dexfutur@users.noreply.github.com>
2022-11-06 13:22:47 -0500

    Initial commit

---
+++ Assets/Scripts/Matrices.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matrices : MonoBehaviour
+{
+    [SerializeField] Vector4 location;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Matrix4x4 matrix = new Matrix4x4(new Vector4(1, 0, 0, 0), new Vector4(1, 0, 0, 0));
+        Vector4 LocalPosition = transform.localPosition; LocalPosition.w = 1;
+        transform.localPosition = matrix * LocalPosition;
+    }
